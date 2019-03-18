a0c307b40084e0327d7f977e6c8877bf4f831d36
BaileyMcInnis <48448925+BaileyMcInnis@users.noreply.github.com>
2019-03-18 11:40:30 -0500

    All the needed things to run it on your own

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Running Rabbit' # Only 10 chars displayed.
-strategy_name = 'Tit for Tat'
-strategy_description = 'If you are nice we are nice'
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+strategy_name = 'The name the team gives to this strategy'
+strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,26 +25,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-   
-    if len(my_history) == 0:
-        return 'c'
-    if len(their_history)[-1] == 'b':
-        return 'b'
-    if len(their_history)[-1] == 'c':
-        return 'c'
-    if len(their_history)[-2] == 'b':
-        return 'b'
-    if len(their_history)[-2] == 'c':
-        return 'c'
-    if len(their_history)[-3] == 'b':
-        return 'b'
-    if len(their_history)[-3] == 'c':
-        return 'c'
+    
     return 'c'
-
-
-
-
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
