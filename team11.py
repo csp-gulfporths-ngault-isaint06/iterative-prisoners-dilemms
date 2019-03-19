57ecb9c012a489e8f44365e0a240b315d00b1cb6
BaileyMcInnis <48448925+BaileyMcInnis@users.noreply.github.com>
2019-03-19 11:53:25 -0500

    why

--- team11.py
+++ team11.py
@@ -26,15 +26,20 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
    
-    if len(my_history) == 0:
-        return 'c'
-    if their_history(-1) == 'b':
+    
+    
+    
+    if 'b' in their_history[-10:]:
         return 'b'
+    else:
+        if random.random()<0.3:
+            return 'b'
+        else:
+            return 'c'
 
 
 
-
-
+import random
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
